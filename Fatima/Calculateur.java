9a11f35c88fc8e0e2dd3558cd3a9cda338e50a01
Fatima Zohra Aboudeine <aboudeinefatimazohra@gmail.com>
2026-04-16 14:24:35 -0400

    Rendre les méthodes statiques pour faciliter l'orchestration dans le main

--- Fatima/Calculateur.java
+++ Fatima/Calculateur.java
@@ -4,7 +4,7 @@ public class Calculateur {
   
      //Calcule la moyenne d'un ensemble de données.
   
-    public double calculerMoyenne(double[] donnees) throws ArithmeticException {
+    public static double calculerMoyenne(double[] donnees) throws ArithmeticException {
         if (donnees == null || donnees.length == 0) { // Déclancher une erreur s'il n'y a pas de données (dénominateur nul)
             throw new ArithmeticException("Erreur : Calcul impossible.");
         }
@@ -18,7 +18,7 @@ public class Calculateur {
         return total / donnees.length;
     }
 
-    public void validerValeur(double valeur) throws IllegalArgumentException {      // Méthode de validation pour que toutes les notes soient positives :
+    public static void validerValeur(double valeur) throws IllegalArgumentException {      // Méthode de validation pour que toutes les notes soient positives :
         if (valeur < 0) { 
             throw new IllegalArgumentException("Erreur : Valeur négative (" + valeur + ") - La note ne peut pas être négative !");
         }
