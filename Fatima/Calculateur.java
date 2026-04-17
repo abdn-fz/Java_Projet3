746ff8da25bc7c933e3cbe88b9d6c5797a7f966b
Fatima Zohra Aboudeine <aboudeinefatimazohra@gmail.com>
2026-04-17 16:33:30 -0400

    Ajout des méthodes min et max et validation des données

--- Fatima/Calculateur.java
+++ Fatima/Calculateur.java
@@ -1,26 +1,47 @@
 package Fatima;
 
 public class Calculateur {
-  
-     //Calcule la moyenne d'un ensemble de données.
-  
-    public static double calculerMoyenne(double[] donnees) throws ArithmeticException {
-        if (donnees == null || donnees.length == 0) { // Déclancher une erreur s'il n'y a pas de données (dénominateur nul)
-            throw new ArithmeticException("Erreur : Calcul impossible.");
-        }
+
+  // Vérifie que les données sont valides
+  private static void verifierDonnees(double[] donnees) {
+    if (donnees == null || donnees.length == 0) {
+      throw new IllegalArgumentException("Le tableau de données est vide.");
+    }
+  }
+    // Moyenne :
+    public static double calculerMoyenne(double[] donnees) {
+        verifierDonnees(donnees);
 
         double total = 0;
         for (double valeur : donnees) {
-            validerValeur(valeur); //  Validation de chaque valeur avant de l'ajouter à la somme
             total += valeur;
         }
 
         return total / donnees.length;
     }
 
-    public static void validerValeur(double valeur) throws IllegalArgumentException {      // Méthode de validation pour que toutes les notes soient positives :
-        if (valeur < 0) { 
-            throw new IllegalArgumentException("Erreur : Valeur négative (" + valeur + ") - La note ne peut pas être négative !");
+  // Trouver le max :
+  public static double calculerMax(double[] donnees) {
+        verifierDonnees(donnees);
+        double max = donnees[0];
+        for (int i = 1; i < donnees.length; i++) {
+            if (donnees[i] > max) {
+                max = donnees[i];
+            }
         }
+        return max;
     }
-}
+
+  // Trouver le min :
+  public static double calculerMin(double[] donnees) {
+      verifierDonnees(donnees);
+
+      double min = donnees[0];
+      for (int i = 1; i < donnees.length; i++) {
+          if (donnees[i] < min) {
+                min = donnees[i];
+            }
+        }
+        return min;
+    }
+    }
