4171c3f00f8a3652708859fa8636d32335dc2065
Wren <127552602+ENU-L1@users.noreply.github.com>
2026-04-22 21:55:42 -0400

    Creation de le Main.java
    L'importation des bibliothèque neccéssaires
    Création du methode main
    Utilisation d'une bloc try/catch

--- Wren/Main.java
+++ Wren/Main.java
@@ -1,1 +1,38 @@
+import java.util.List;
 
+public class Main {
+  public static void main(String[] args) {
+    String fichierEntree = "donnees.csv";
+    String fichierSortie = "resume.csv";
+
+    // On crée l'objet ici (car on utilise une interface)
+    Calculateur calc = new Calculateur();
+
+    try {
+      String[] noms = LecteurCSV.lireNomsColonnes(fichierEntree);
+      List<double[]> donnees = LecteurCSV.lireColonnes(fichierEntree);
+
+      if (donnees.isEmpty()) return;
+
+      System.out.println("--- ANALYSE EN COURS ---");
+
+      for (int = 0; i < donnees. size(); i++) {
+        double[] col = donnees.get(i);
+        String titre = (i < noms.length) ? noms[i] : "Col " + i;
+
+        // Utilisation de l'objet 'calc'
+        System.out.println("\nNom: " + titre);
+        System.out.printf("Moyenne: %.2f | Minimum: %.1f | Maximum: %.1f%n",
+                          calc.calculerMoyenne(col),
+                          calc.calculerMin(col),
+                          calc.calculerMax(col));
+      }
+
+
+
+      
+    } catch (Exception e) {
+      System.err.println("Erreur: " + e.getMessage());
+    }
+  }
+}
